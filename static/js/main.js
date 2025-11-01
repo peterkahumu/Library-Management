32684200e844fedab151fb4e03b860329099fc26
peterkahumu <muhumukip@gmail.com>
2025-11-01 07:37:51 +0300

    fix: null check, incomplete django url tag

--- static/js/main.js
+++ static/js/main.js
@@ -7,5 +7,7 @@ document.addEventListener("DOMContentLoaded", (e) => {
 
     // for date of birth field, remove default value.
     const date_of_birth = document.querySelector("#id_date_of_birth")
-    date_of_birth.value = ""
+    if (date_of_birth) {
+        date_of_birth.value = ""
+    }
 })
