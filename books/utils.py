0313b18ce2ba3a7a0e2bd6bef32c17e2a17d0e05
peterkahumu <muhumukip@gmail.com>
2025-12-13 19:11:54 +0300

    refactor: place the docstring in invalidate_cache_method just after the function signature

--- books/utils.py
+++ books/utils.py
@@ -1,8 +1,8 @@
 def invalidate_cache():
-    from django.core.cache import cache
-
     """
     Clear all cache related to books on books creation, update and/or deletion
     """
+    from django.core.cache import cache
+
     cache.delete("stats:total_books")
     cache.delete("stats:available_books")
