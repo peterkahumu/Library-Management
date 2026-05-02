9bd9f17f5909182cd08bf788a17911fc8a57fd08
Peter Muhumuki <muhumukip@gmail.com>
2026-05-02 05:48:27 +0300

    refacor: remove automatic genre creation via signal

--- books/signals.py
+++ books/signals.py
@@ -1,16 +1,8 @@
-from django.db.models.signals import post_migrate, post_save, post_delete
+from django.db.models.signals import post_save, post_delete
 from django.dispatch import receiver
 from .utils import invalidate_cache
 
 from .models import Genre, Book
-from .constants import GENRE_CHOICES
-
-
-@receiver(post_migrate)
-def create_genres(sender, **kwargs):
-    if sender.label == "books":
-        for value, _ in GENRE_CHOICES:
-            Genre.objects.get_or_create(name=value)
 
 
 @receiver([post_save, post_delete], sender=Book)
